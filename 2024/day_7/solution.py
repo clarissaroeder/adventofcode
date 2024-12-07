208b9ee67f427da559a373105516896ab1d0de0e
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-12-07 02:14:58 -0500

    refactor

--- 2024/day_7/solution.py
+++ 2024/day_7/solution.py
@@ -34,38 +34,18 @@ class Advent:
             current_result = self.calculate(result, numbers[i], operator)
             if current_result > target:
                 return False
-            
-            # Try continuing with the same operator
-            if dfs(current_result, i + 1, operator):
-                return True
-            
-            # Try other operators
+   
             for new_operator in operators:
-                if new_operator != operator:
-                    if dfs(current_result, i + 1, new_operator):
-                        return True
+                if dfs(current_result, i + 1, new_operator):
+                    return True
                     
             return False
 
         return any(dfs(numbers[0], 1, operator) for operator in operators)
 
-
-    def solve_part1(self):
-        total = 0
-        for t, nums in self.data.items():
-            if self.solvable(t, nums, self.base_operators):
-                total += t
-            else:
-                self.unsolvable[t] = nums
-        print("part 1:", total)
-        return total
-    
-    def solve_part2(self):
-        total = sum(t for t, nums in self.unsolvable.items() if self.solvable(t, nums, self.extended_operators))
-        return total
-
     def solve(self):
-        sum1 = self.solve_part1()
-        sum2 = self.solve_part2()
-        print("part 2:", sum1 + sum2)
+        total1 = sum(t for t, nums in self.data.items() if self.solvable(t, nums, self.base_operators))
+        print("part 1:", total1)
+        total2 = sum(t for t, nums in self.data.items() if self.solvable(t, nums, self.extended_operators))
+        print("part 2:", total2)
 
